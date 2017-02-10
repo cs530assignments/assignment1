86f821072bc6fa2f7bba65936bdaf2d629f7d706
cs530assignments <cs530assignments@gmail.com>
2017-02-09 16:34:33 -0800

    Update assign1.cpp

--- assign1.cpp
+++ assign1.cpp
@@ -7,55 +7,84 @@
 
 using namespace std;
 
-/* 
-  Determines if the given character is a valid ascii character
-  by checking the printable ascii codes
-*/
+/*
+ Determines if the given character is a valid ascii character
+ by checking the printable ascii codes
+ */
 bool isValidASCII(char c) {
- if ((c - '0') > 31 || (c - '0') < 127) {
-  return true;
- } 
- return false;
+    if (c  > 31 && c < 127) {
+        return true;
+    }
+    return false;
 }
-
-/* 
-  Prints the valid ascii characters from the current line of the input file
-*/
-void printValidASCII (string str) {
-  for (unsigned int i = 0; i < str.length(); i++) {
-    if (isValidASCII(str[i])) {
-      cout << str[i];
+/*
+ Prints the address associated with the line of the file 
+ */
+void printStringAddr(int addr) {
+    cout << setfill('0') << setw(7) << hex << addr;
+    cout << ":";
+}
+/*
+ Prints the hex codes from the given input file.
+ */
+void printHexString(char* str, long len) {
+    for (unsigned int i = 0; i < len; i++) {
+        if (i % 2 == 0) {
+            cout << " ";
+        }
+        cout << setfill('0') << setw(2) << hex << static_cast<int>(str[i]);
+    }
+    long spaces = 0;
+    string s;
+    if (len < 16) {
+        spaces = 41 - (len*2 + len/2 + 1);
+        s = string(spaces,' ');
     } else {
-      cout << ".";
+        s = " ";
     }
-  }
+    
+    cout << s;
 }
-
 /*
-  Prints the hex codes for the given line of the input file
-*/
-void printHexSolution(char *argv[]) {
-  ifstream file(argv[1]);
-  string currentLine;
-  int word = 0;
-  while (getline(file, currentLine)) {
-    for (unsigned int i = 0; i < currentLine.length(); i++) {
-      if (i % 2 == 0) {
-        cout << " ";
-      }
-      cout << hex << static_cast<int>(currentLine[i]);
+ Prints the valid ascii characters from the current line of the input file
+ */
+void printValidASCII (char* str, long len) {
+    for (unsigned int i = 0; i < len; i++) {
+        if (isValidASCII(str[i])) {
+            cout << str[i];
+        } else {
+            cout << ".";
+        }
     }
-    cout << " ";
-    printValidASCII(currentLine);
-    cout << '\n';
-  }
+    cout << endl;
 }
-
 /*
-  Prints solution when user invokes the "-b"  flag
-*/
+ Prints the entire hexdump output if hex is requested output style
+ */
+void printHexSolution(char *fname) {
+    ifstream file(fname, std::ios::binary);
+    char currentSixteen[16];
+    int addr = 0;
+    long charCount;
+    while (file.read(currentSixteen, 16) ) {
+       charCount = file.gcount();
+       printStringAddr(addr);
+       printHexString(currentSixteen, charCount);
+       printValidASCII(currentSixteen, charCount);
+       addr+=16;
+    }
+    charCount = file.gcount();
+    if (charCount > 0) {
+        printStringAddr(addr);
+        printHexString(currentSixteen, charCount);
+        printValidASCII(currentSixteen, charCount);
+    }
+}
+/*
+ Prints solution when user invokes the "-b"  flag
+ */
 // void printBinarySolution(char *argv[]) {
-//   ifstream file(argv[2]);
+//   ifstream file(argv[2], binary);
 //   string currentLine;
 //   while (getline(file, currentLine)) {
 //     for (unsigned int i = 0; i < currentLine.length(); i++) {
@@ -63,47 +92,41 @@ void printHexSolution(char *argv[]) {
 //         cout << '\n';
 //       }
 //       bitset<8> characterInBinary = currentLine[i];
-//       cout << characterInBinary << " "; 
+//       cout << characterInBinary << " ";
 //     }
 //     printValidASCII(currentLine);
 //     cout << '\n';
 //   }
 // }
 
-bool printableASCII(char ASCIICode) {
-  if ((ASCIICode - '0') > 31 || (ASCIICode - '0') < 127) {
-    return true;
-  }
-  return false;
-}
-
-void printBinarySolution(char *argv[]) {
-  ifstream file(argv[2]);
-  string currentLine;
-  string validASCII;
-  while (getline(file, currentLine)) {
-    for (unsigned int i = 0; i < currentLine.length(); i++) {
-      if (i > 0 && i % 6 == 0) {
-        cout << validASCII << endl;
-        validASCII = "";
-      }
-      bitset<8> characterInBinary = currentLine[i];
-      cout << characterInBinary << ' ';
-      if (printableASCII(currentLine[i])) {
-        validASCII = validASCII + currentLine[i];
-      }
-      if (i == currentLine.length() - 1) {
-        cout << validASCII << endl;
-        break;
-      }
+void printBinarySolution(char *fname) {
+    ifstream file(fname, std::ios::binary);
+    string currentLine;
+    string validASCII;
+    while (getline(file, currentLine)) {
+        for (unsigned int i = 0; i < currentLine.length(); i++) {
+            if (i > 0 && i % 6 == 0) {
+                cout << validASCII << endl;
+                validASCII = "";
+            }
+            bitset<8> characterInBinary = currentLine[i];
+            cout << characterInBinary << ' ';
+            if (isValidASCII(currentLine[i])) {
+                validASCII = validASCII + currentLine[i];
+            }
+            if (i == currentLine.length() - 1) {
+                cout << validASCII << endl;
+                break;
+            }
+        }
     }
-  }
 }
 
 int main(int argc, char *argv[]) {
-  if (strcmp(argv[1], "-b") == 0) {
-    printBinarySolution(argv);
-  } else {
-    printHexSolution(argv);
-  }
+    if (strcmp(argv[1], "-b") == 0) {
+        printBinarySolution(argv[2]);
+    } else {
+        printHexSolution(argv[1]);
+    }
+    cin.get();
 }
