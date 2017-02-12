9e9ea8f6f707c5cd68b704eec78b7ef5952c1245
cs530assignments <cs530assignments@gmail.com>
2017-02-12 14:16:06 -0800

    Create header.h
    header file we needed to add, it just has all the includes in it that assignment1.cpp uses

---
+++ header.h
@@ -0,0 +1,18 @@
+//
+//  header.h
+//  a1
+//
+//  Created by James Hammon on 2/10/17.
+//  Copyright © 2017 James Hammon. All rights reserved.
+
+#ifndef header_h
+#define header_h
+
+#include <iostream> /* Library provides functions to print to the screen */
+#include <fstream> /* Library provides functions to interact with files */
+#include <string> /* Library provides ability to use the string type */
+#include <iomanip> /* Library allows for hex printing */
+#include <cstring> /* Library provides function to compare strings/chars */
+#include <bitset> /* Library for printing out binary */
+
+#endif /* Header_h */
